a1225c4bd0c6cf72f3c093640a166da9e74db7f5
KariVI <47987287+KariVI@users.noreply.github.com>
2021-10-28 09:24:12 -0500

    Add validation

---
+++ AdivinaQue.Client/AdivinaQue.Client/Control/Validation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdivinaQue.Client.Control
+{
+    public class Validation
+    {
+        public  bool validateEmail(string email)
+        {
+            bool value = true;
+            try
+            {
+                MailAddress m = new MailAddress(email);
+
+            }
+            catch (FormatException ex)
+            {
+                value = false;
+            }
+            return value;
+        }
+    }
+}
+
